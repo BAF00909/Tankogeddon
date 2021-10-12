108034d22fb35481f7438888fcb923278e0bf94c
Alexander Breus <Lis00909@gmail.com>
2021-10-12 20:55:54 +0300

    Добавлена турель

---
+++ Source/Tankogeddon/Turret.cpp
@@ -0,0 +1,103 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Turret.h"
+#include "Components/StaticMeshComponent.h"
+#include "TankPlayerController.h"
+#include "Cannon.h"
+#include "Kismet/KismetMathLibrary.h"
+#include "TimerManager.h"
+#include "Components/ArrowComponent.h"
+#include "Components/BoxComponent.h"
+
+// Sets default values
+ATurret::ATurret()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	BodyMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("BodyMesh"));
+	RootComponent = BodyMesh;
+
+	TurretMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("TurretMesh"));
+	TurretMesh->AttachToComponent(BodyMesh, FAttachmentTransformRules::KeepRelativeTransform);
+
+	CannonSetupPoint = CreateDefaultSubobject<UArrowComponent>(TEXT("Cannon setup point"));
+	CannonSetupPoint->AttachToComponent(TurretMesh, FAttachmentTransformRules::KeepRelativeTransform);
+
+	HitCollider = CreateDefaultSubobject<UBoxComponent>(TEXT("Hit collider"));
+	HitCollider->SetupAttachment(BodyMesh);
+}
+
+// Called when the game starts or when spawned
+void ATurret::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	FActorSpawnParameters Params;
+	Params.Owner = this;
+	Cannon = GetWorld()->SpawnActor<ACannon>(CannonClass, Params);
+	Cannon->AttachToComponent(CannonSetupPoint, FAttachmentTransformRules::SnapToTargetNotIncludingScale);
+
+	PlayerPawn = GetWorld()->GetFirstPlayerController()->GetPawn();
+
+	FTimerHandle TargetingTimerHandle;
+	GetWorld()->GetTimerManager().SetTimer(TargetingTimerHandle, this, &ATurret::Targeting, TargetingRate, true, TargetingRate);
+}
+
+// Called every frame
+void ATurret::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void ATurret::Destroyed() 
+{
+	if (Cannon)
+	{
+		Cannon->Destroy();
+	}
+};
+
+void ATurret::Targeting() 
+{
+	if (IsPlayerInRange())
+	{
+		RotateToPlayer();
+	}
+
+	if (CanFire() && Cannon && Cannon->IsReadyToFire())
+	{
+		Fire();
+	}
+};
+
+void ATurret::RotateToPlayer() 
+{
+	FRotator TargetRotation = UKismetMathLibrary::FindLookAtRotation(GetActorLocation(), PlayerPawn->GetActorLocation());
+	FRotator CurrentRotation = TurretMesh->GetComponentRotation();
+	TargetRotation.Pitch = CurrentRotation.Pitch;
+	TargetRotation.Roll = CurrentRotation.Roll;
+	TurretMesh->SetWorldRotation(FMath::RInterpConstantTo(CurrentRotation, TargetRotation, GetWorld()->GetDeltaSeconds(), TargetingSpeed));
+};
+
+bool ATurret::IsPlayerInRange() 
+{
+	return FVector::Distance(PlayerPawn->GetActorLocation(), GetActorLocation()) <= TargetingRange;
+};
+
+bool ATurret::CanFire() 
+{
+	FVector TargetingDir = TurretMesh->GetForwardVector();
+	FVector DirToPlayer = PlayerPawn->GetActorLocation() - GetActorLocation();
+	DirToPlayer.Normalize();
+	float DirAngle = FMath::RadiansToDegrees(acosf(FVector::DotProduct(TargetingDir,DirToPlayer)));
+	return DirAngle <= Accurency;
+};
+void ATurret::Fire() 
+{
+	if (Cannon)
+	{
+		Cannon->Fire();
+	}
+};
---
+++ Source/Tankogeddon/Turret.h
@@ -0,0 +1,68 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Turret.generated.h"
+
+UCLASS()
+class TANKOGEDDON_API ATurret : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ATurret();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	virtual void Destroyed() override;
+	void Targeting();
+	void RotateToPlayer();
+	bool IsPlayerInRange();
+	bool CanFire();
+	void Fire();
+
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+protected:
+
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadWrite, Category ="Components")
+	class UStaticMeshComponent* BodyMesh;
+
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadWrite, Category = "Components")
+	class UStaticMeshComponent* TurretMesh;
+
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadWrite, Category = "Components")
+	class UArrowComponent* CannonSetupPoint;
+
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadWrite, Category = "Components")
+	class UBoxComponent* HitCollider;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Ammo")
+	TSubclassOf<class ACannon> CannonClass;
+
+	UPROPERTY()
+	class ACannon* Cannon;
+
+	UPROPERTY()
+	class APawn* PlayerPawn;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Target")
+	float TargetingRange = 1000.0f;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Target")
+	float TargetingSpeed = 0.1f;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Target")
+	float TargetingRate = 0.005f;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Target")
+	float Accurency = 10.0f;
+	
+};
